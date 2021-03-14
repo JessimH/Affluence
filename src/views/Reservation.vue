1b9962f54ea9ae48e6aa32ef64d2626223e154ad
Jessdev <61420136+JessimH@users.noreply.github.com>
2021-03-14 14:51:14 +0100

    first commit

--- src/views/Reservation.vue
+++ src/views/Reservation.vue
@@ -2,27 +2,27 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Reservation</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Reservation</ion-title>
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 3 page" />
+      <ExploreContainer name="Reservation" />
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
-  name: 'Tab3',
+  name: 'Reservation',
   components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 }
 </script>
